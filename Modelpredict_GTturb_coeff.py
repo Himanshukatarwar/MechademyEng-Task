8ef51ba84e1772806b52aa4bf505973db3e3a16b
Himanshukatarwar <53312035+Himanshukatarwar@users.noreply.github.com>
2020-11-19 20:51:48 +0530

    Update Modelpredict_GTturb_coeff.py

--- Modelpredict_GTturb_coeff.py
+++ Modelpredict_GTturb_coeff.py
@@ -3,7 +3,7 @@ from joblib import load
 class Model(object):
     def __init__(self):
         self.normalize = Normalize()
-        self.model = load('trained model/TaskA_bestmodel.joblib')
+        self.model = load('TrainedModel/TaskA_bestmodel.joblib')
     def predict(self,data):
         data = Normalize().transform(data,'GTturb')
         return self.model.predict(data)
